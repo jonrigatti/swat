57d93fce8e01b4202d236a3ed69cf8ac39972bae
jonrigatti <jon.rigatti@live.com>
2023-08-07 16:27:49 -0400

    Gogogo

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createPinia, PiniaVuePlugin } from 'pinia';
-import VueCompositionAPI from '@vue/composition-api'
 
-Vue.use(VueCompositionAPI)
+// Vue.use(VueCompositionAPI)
 Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
 
