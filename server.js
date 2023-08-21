43cdcd786419864b775906bd69bc91558de5d345
jonrigatti <jon.rigatti@live.com>
2023-08-21 12:23:55 -0400

    Stuff

--- server.js
+++ server.js
@@ -1,55 +1,85 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require ("cors");
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-const app = express();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-};
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+})
 
-app.use(cors(corsOptions));
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+})
 
-// parse requests of content-type - applicaton/json
-app.use(bodyParser.json());
+// Use default router
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require ("cors");
+
+// const app = express();
+
+// var corsOptions = {
+//     origin: "http://localhost:8081"
+// };
+
+// app.use(cors(corsOptions));
+
+// // parse requests of content-type - applicaton/json
+// app.use(bodyParser.json());
 
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to the thing." });
-});
-
-// app.get("/testdir", (req, res) => {
-//         fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-//             if (err) throw err;
-//             console.log("folder created ...");
-//         });
-//         res.json({ message: "Test directory created." });
+// // simple route
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome to the thing." });
 // });
 
-require("./routes/submittal.routes")(app);
-require("./routes/projects.routes")(app);
-require("./routes/server.routes")(app);
+// // app.get("/testdir", (req, res) => {
+// //         fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+// //             if (err) throw err;
+// //             console.log("folder created ...");
+// //         });
+// //         res.json({ message: "Test directory created." });
+// // });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+// require("./routes/submittal.routes")(app);
+// require("./routes/projects.routes")(app);
+// require("./routes/server.routes")(app);
 
-const db = require("./models");
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
-    });
+// // set port, listen for requests
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`);
+// });
+
+// const db = require("./models");
+// db.mongoose
+//     .connect(db.url, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to the database!");
+//     })
+//     .catch(err => {
+//         console.log("Cannot connect to the database!", err);
+//         process.exit();
+//     });
